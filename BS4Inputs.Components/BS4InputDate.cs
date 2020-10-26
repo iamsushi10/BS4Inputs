6566e6d7caa8d5cfee517f01e593ef36502be3ae
Edward Lyndon Madali <edwardlyndonmadali@gmail.com>
2020-10-26 23:04:07 +0800

    Switched BS4InputDate razor component to a class because it can't accept nullable datetime values.

---
+++ BS4Inputs.Components/BS4InputDate.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BS4Inputs.Components
+{
+    public class BS4InputDate<TValue> : InputDate<TValue>
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, "input");
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddAttribute(2, "type", "date");
+            builder.AddAttribute(3, "class", BS4Input.FixClassNames(CssClass));
+            builder.AddAttribute(4, "value", BindConverter.FormatValue(CurrentValueAsString));
+            builder.AddAttribute(5, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
+            builder.CloseElement();
+        }
+    }
+}
