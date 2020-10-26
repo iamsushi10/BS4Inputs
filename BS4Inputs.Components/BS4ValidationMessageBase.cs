06f7f123bf81b6b268dba5f8b88f738a1387e543
Edward Lyndon Madali <edwardlyndonmadali@gmail.com>
2020-10-26 15:42:09 +0800

    Added a customizable validation message razor component based on Chris Sainty's solution.

---
+++ BS4Inputs.Components/BS4ValidationMessageBase.cs
@@ -0,0 +1,35 @@
+﻿// Code sourced from this blog: https://chrissainty.com/creating-a-custom-validation-message-component-for-blazor-forms/
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Microsoft.AspNetCore.Components.Forms
+{
+    public class BS4ValidationMessageBase<TValue> : ComponentBase, IDisposable
+    {
+        private FieldIdentifier _fieldIdentifier;
+
+        [CascadingParameter] EditContext EditContext { get; set; }
+        [Parameter] public Expression<Func<TValue>> For { get; set; }
+        [Parameter] public string Class { get; set; }
+
+        protected IEnumerable<string> ValidationMessages => EditContext.GetValidationMessages(_fieldIdentifier);
+
+        protected override void OnInitialized()
+        {
+            _fieldIdentifier = FieldIdentifier.Create(For);
+            EditContext.OnValidationStateChanged += HandleValidationStateChanged;
+        }
+
+        private void HandleValidationStateChanged(object sender, ValidationStateChangedEventArgs eventArgs)
+        {
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            EditContext.OnValidationStateChanged -= HandleValidationStateChanged;
+        }
+    }
+}
